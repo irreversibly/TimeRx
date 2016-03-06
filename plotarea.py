aeb731b35a56eb7fb37233a0896bea4a6df862ce
Priyanka Vijay <pv225@cornell.edu>
2016-03-05 19:52:12 -0500

    Update plotarea.py

--- plotarea.py
+++ plotarea.py
@@ -2,13 +2,14 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 from bokeh.charts import Area
-from bokeh.plotting import output_file
+from bokeh.plotting import output_file, show
 # sns.set_palette('dark')
 
 
 def plot(concdf, outputfile):
-    Area(concdf, legend='top_left',
-         title="Plasma Concentration over Time",
-         xlabel="Time", ylabel="Concentration")
+    area = Area(concdf, legend='top_left',
+                title="Plasma Concentration over Time",
+                xlabel="Time", ylabel="Concentration")
     output_file(outputfile, title="Concentraion Plot")
+    show(area)
     return
