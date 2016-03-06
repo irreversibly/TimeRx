f341457b94787b9d1a94100a42856b1a70f4afd4
Priyanka Vijay <pv225@cornell.edu>
2016-03-05 21:57:51 -0500

    feed user input to kinetics, plotting, and report gen functions

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import pandas as pd
+
+import sys
+sys.path.insert(0, './kinetizer')
+sys.path.insert(0, './plotter')
 from plotarea import plot
+# from kinetizer import *
 
 app = Flask(__name__)
+
+# placeholders for functions to be imported
+def rafalsfunction(list):
+    return None, None
+def druvasfunction():
+    pass
 
 
 @app.route('/')
@@ -13,11 +25,29 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def form():
+    name = request.form['name']
     drug1 = request.form['drug1']
     dose1 = request.form['dose1']
-    processed_text = drug1 + dose1
-    return processed_text
-    # return render_template('form.html')
+    drug2 = request.form['drug2']
+    dose2 = request.form['dose2']
+    drug3 = request.form['drug3']
+    dose3 = request.form['dose3']
+    drug4 = request.form['drug4']
+    dose4 = request.form['dose4']
+
+    # check if drugs are specified
+    drugsinput = []
+    for drug in [drug1, drug2, drug3, drug4]:
+        if len(drug) > 0:
+            drugsinput.append(drug)
+
+    concdict, scheddict = rafalsfunction(drugsinput)
+    concdf = pd.DataFrame.from_dict(concdict)
+    plot(concdf, "areaplot.html")
+    druvasfunction(scheddict, "areaplot.html", "report.html")
+    # processed_text = drug1 + dose1
+    # return processed_text
+    return render_template('report.html')
 
 
 if __name__ == '__main__':
