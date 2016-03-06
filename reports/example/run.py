e81d854a82afeb0ff219e5b9fda2a0efa86efd5d
dc1340 <dc1340@gmail.com>
2016-03-06 00:25:17 -0500

    Working Reports
    Yay!

--- reports/example/run.py
+++ reports/example/run.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('img_template.html', my_string="Dieee!", my_list=[0,1,2,3,4,5])
-    #return render_template('template.html', my_string="Dieee!", my_list=[0,1,2,3,4,5])
-
+    #return render_template('img_template.html', my_string="Dieee!", my_list=[0,1,2,3,4,5])
+    return render_template('img_template.html', my_string="T4!", my_list=[0,1,2,3,4,5])
+    #return render_template('img_template.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
