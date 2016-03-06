7bb5c6586282a1df6c984ebec15c392d869653e2
priya-vijay <priyaaa.v@gmail.com>
2016-03-06 05:41:28 -0500

    testing...

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import pandas as pd
+from pandas import read_csv
 import os
 import sys
 sys.path.insert(0, './kinetizer')
@@ -59,7 +60,24 @@ def form():
     # druvasfunction("templates/schedule.csv", "templates/areaplot.html", "report.html")
     # processed_text = drug1 + dose1
     # return processed_text
-    return render_template("areaplot.html")
+    make_report('static/plot4.html', 'static/example_table.txt')
+    return render_template("report.html")
+
+
+@app.route('/report')
+def make_report(plot_file, schedule_file):
+    # plot_file = 'static/plot4.html'
+    # schedule_file = 'static/example_table.txt'
+    #
+    #
+    # if len(sys.argv)==3:
+    #     plot_file=sys.argv[2]
+    #     schedule_file=sys.argv[1]
+    # elif len(sys.argv)==2:
+    #     schedule_file=sys.argv[1]
+
+    schedule = read_csv(schedule_file, delimiter="\t")
+    return render_template('Template_Report.html', schedule_table=schedule, plot=plot_file, name="report")
 
 
 if __name__ == '__main__':
